323f1396e7aa70eac54871453f917ed0f261ce8f
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-17 13:37:52 -0300

    chore: add chevron-right svg

--- packages/mobile/src/assets/icons/index.ts
+++ packages/mobile/src/assets/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as Rentx } from './rentx.svg';
 export { default as Calendar } from './calendar.svg';
 export { default as Car } from './car.svg';
-export { default as Rentx } from './rentx.svg';
+export { default as ChevronRight } from './chevron-right.svg';
