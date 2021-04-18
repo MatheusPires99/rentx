63d1e117b9cc501f4ecc4a5dda07a560f0c07ab0
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-18 10:23:12 -0300

    feat: finish Checkbox component

--- packages/mobile/src/components/atoms/Checkbox/index.tsx
+++ packages/mobile/src/components/atoms/Checkbox/index.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
+
+import {
+  useSharedValue,
+  withTiming,
+  Easing,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolate,
+} from 'react-native-reanimated';
 
 import * as S from './styles';
 
@@ -10,10 +19,49 @@ type CheckboxProps = {
 };
 
 export const Checkbox = ({ isChecked, onChange, label }: CheckboxProps) => {
+  const checkedBoxOpacity = useSharedValue(0);
+
+  useEffect(() => {
+    checkedBoxOpacity.value = withTiming(isChecked ? 1 : 0, {
+      duration: 250,
+      easing: Easing.bezier(0.19, 1, 0.22, 1),
+    });
+  }, [isChecked, checkedBoxOpacity]);
+
+  const backgroundStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: interpolate(
+            checkedBoxOpacity.value,
+            [0, 1],
+            [0, 1],
+            Extrapolate.CLAMP,
+          ),
+        },
+      ],
+    };
+  });
+
+  const checkedBoxStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        checkedBoxOpacity.value,
+        [0.5, 1],
+        [0, 1],
+        Extrapolate.CLAMP,
+      ),
+    };
+  });
+
   return (
     <TouchableWithoutFeedback onPress={onChange}>
       <S.Container>
-        <S.Box isChecked={isChecked}>{isChecked && <S.CheckedBox />}</S.Box>
+        <S.Box>
+          <S.Background style={backgroundStyle} />
+
+          <S.CheckedBox style={checkedBoxStyle} />
+        </S.Box>
 
         <S.Label>{label}</S.Label>
       </S.Container>
--- packages/mobile/src/components/atoms/Checkbox/styles.ts
+++ packages/mobile/src/components/atoms/Checkbox/styles.ts
@@ -1,8 +1,5 @@
 import styled, { css } from 'styled-components/native';
-
-type BoxProps = {
-  isChecked: boolean;
-};
+import Animated from 'react-native-reanimated';
 
 export const Container = styled.View`
   ${() => css`
@@ -11,19 +8,36 @@ export const Container = styled.View`
   `}
 `;
 
-export const Box = styled.View<BoxProps>`
-  ${({ theme, isChecked }) => css`
+export const Box = styled.View`
+  ${({ theme }) => css`
     width: 20px;
     height: 20px;
-    background: ${isChecked ? theme.colors.black : theme.colors.gray['100']};
+    background: ${theme.colors.gray['100']};
     margin-right: 12px;
+
+    position: relative;
 
     align-items: center;
     justify-content: center;
   `}
 `;
 
-export const CheckedBox = styled.View`
+export const Background = styled(Animated.View)`
+  ${({ theme }) => css`
+    background: ${theme.colors.black};
+
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
+  `}
+`;
+
+export const CheckedBox = styled(Animated.View)`
   ${({ theme }) => css`
     width: 8px;
     height: 8px;
